73f16e3803a31bee1d502c0a3452a1f6cea6ddc7
douya <1712910797@qq.com>
2023-04-02 23:02:34 +0800

    hot-fix commit1

--- src/main/java/com/douya/git/GitTest.java
+++ src/main/java/com/douya/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hhh");
         System.out.println("hhh");
         System.out.println("hhh");
+        System.out.println("hhh hot-fix");
     }
 }
